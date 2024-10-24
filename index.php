199eda60d80d3f5cad85d91439ec3756079ee65f
megabyez <buffnivea001@gmail.com>
2024-10-25 02:08:27 +0700

    Add link input form

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-// Tệp chính để hiển thị trang chủ của ứng dụng
-echo "Welcome to my affiliate link converter!";
+// Chuyển hướng từ megabye.online sang www.megabye.online nếu cần
+if ($_SERVER['HTTP_HOST'] == 'megabye.online') {
+    header("Location: https://www.megabye.online".$_SERVER['REQUEST_URI'], true, 301);
+    exit();
+}
+
+// Giao diện HTML để nhập link
+echo '
+<html>
+<head>
+    <title>Affiliate Link Converter</title>
+</head>
+<body>
+    <h1>Welcome to my affiliate link converter!</h1>
+    <form action="your_php_script.php" method="GET">
+        <label for="link">Enter your affiliate link:</label>
+        <input type="text" id="link" name="link" required>
+        <input type="submit" value="Convert Link">
+    </form>
+</body>
+</html>
+';
 ?>
