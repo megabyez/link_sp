bc192404e924b7de81c08541273de8bd68a4081e
megabyez <buffnivea001@gmail.com>
2024-10-25 21:38:27 +0700

    Cập nhật convert.php để thay thế link cũ thành link mới

--- convert.php
+++ convert.php
@@ -1,6 +1,6 @@
 <?php
 // Mã affiliate của bạn
-$aff_code = '17396870089';
+$aff_code = '17396870089'; // Mã affiliate
 
 // Kiểm tra xem có đoạn văn bản nào được gửi tới không
 if (isset($_POST['text'])) {
@@ -8,19 +8,18 @@ if (isset($_POST['text'])) {
 
     // Hàm chuyển đổi link
     $converted_text = preg_replace_callback(
-        '/https:\/\/vn\.shp\.ee\/[^\s]+/', // Tìm các link Shopee bắt đầu bằng https://vn.shp.ee/
+        '/https:\/\/s\.shopee\.vn\/[^\s]+/', // Tìm các link Shopee bắt đầu bằng https://s.shopee.vn/
         function($matches) use ($aff_code) {
             $original_url = $matches[0]; // Link gốc Shopee
 
-            // Thêm mã affiliate vào link Shopee
-            $separator = (strpos($original_url, '?') === false) ? '?' : '&';
-            $url_with_aff = $original_url . $separator . 'aff=' . $aff_code;
-
             // Tạo mã rút gọn cho link có mã affiliate
-            $short_code = substr(md5($url_with_aff), 0, 6); // Tạo mã rút gọn 6 ký tự
+            $short_code = substr(md5($original_url), 0, 6); // Tạo mã rút gọn 6 ký tự
+
+            // Tạo link mới với mã affiliate
+            $new_url = "https://megabye.online/" . $short_code;
 
             // Trả về link rút gọn hoàn chỉnh với mã affiliate đã được thêm vào URL Shopee
-            return "https://megabye.online/" . $short_code;
+            return $new_url;
         },
         $text
     );
